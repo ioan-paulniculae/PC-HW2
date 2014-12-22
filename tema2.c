c010c4dbe71945896bd7fdd52a4218d7aba8c209
Niculae Ioan-Paul <niculae.paul@gmail.com>
2014-12-22 23:51:10 +0200

    Fixed a bug in FoodGen that frozen the game

--- tema2.c
+++ tema2.c
@@ -78,7 +78,6 @@ unsigned main_menu(){
     mvaddstr(p.y - 1, p.x, "OPTIONS");
     mvaddstr(p.y, p.x, "EXIT");
 
-    unsigned op;
     char c;
 
     while(NO_STOP){
@@ -150,7 +149,7 @@ unsigned main_menu(){
     }
 
     clear();
-
+    return 1;
 }
 
 
@@ -214,308 +213,305 @@ int main(){
 
     keypad(stdscr, TRUE);
     init_window();
+
+
+    op = main_menu();
+
+
     getmaxyx(wnd, gboard.nr_y, gboard.nr_x);
-
-    op = main_menu();
-
-
-
-    while (NO_STOP){
-    op = main_menu();
-    switch (op){
-{
-        //THE GAME
-        case 2:
-
-        /*
+    /*
         folosim efectiv un chenar de dimenisuni mai mici
         pentru aspect
-        */
-        gboard.nr_x--;
-        gboard.nr_y--;
+    */
+
+    gboard.nr_x--;
+    gboard.nr_y--;
 
 
+    create_gameboard(gboard.nr_x, gboard.nr_y);
+
+    //din cauza chenarului
+    //dimensiunea efectiva s-a redus iar
+    gboard.nr_x -= 2;
+    gboard.nr_y -= 2;
+
+    snake.p = (PUNCT *) malloc (gboard.nr_x * gboard.nr_y * sizeof (PUNCT));
+
+    //pozitionare initiala
+    snake.p[HEAD].x = gboard.nr_x  / 3;
+    snake.p[HEAD].y = gboard.nr_y / 3 ;
+    mvaddch(snake.p[HEAD].y, snake.p[HEAD].x, 'O');
+
+    //dimensiune initiala
+    snake.dim = 1;
+
+    //viteza initiala
+    snake.speed = 8 * (gboard.nr_x + gboard.nr_y);
+
+    snake.lastinput = 'd';
+
+    while (NO_STOP){
+
+        if (op != 2){
+            endwin();
+            exit(1);
+        }
+        mverr = 0;
+
+        //actualizare coordonate
+        for (i = snake.dim; i > 0; i--){
+            snake.p[i].x = snake.p[i - 1].x;
+            snake.p[i].y = snake.p[i - 1].y;
+        }
+
+        snake.last_x = snake.p[snake.dim].x;
+        snake.last_y = snake.p[snake.dim].y;
+
+        timeout(UNITY - snake.speed);
 
 
+        //adaugare mancare
+        if(hungry){
 
-        create_gameboard(gboard.nr_x, gboard.nr_y);
+            hungry = 0;
+            srand(time(NULL));
 
-        //din cauza chenarului
-        //dimensiunea efectiva s-a redus iar
-        gboard.nr_x -= 2;
-        gboard.nr_y -= 2;
+            food.x = rand() % gboard.nr_x;
+            food.y = rand() % gboard.nr_y;
 
-        snake.p = (PUNCT *) malloc (gboard.nr_x * gboard.nr_y * sizeof (PUNCT));
-
-        //pozitionare initiala
-        snake.p[HEAD].x = gboard.nr_x  / 3;
-        snake.p[HEAD].y = gboard.nr_y / 3 ;
-        mvaddch(snake.p[HEAD].y, snake.p[HEAD].x, 'O');
-
-        //dimensiune initiala
-        snake.dim = 1;
-
-        //viteza initiala
-        snake.speed = 8 * (gboard.nr_x + gboard.nr_y);
-
-        snake.lastinput = 'd';
-
-        while (NO_STOP){
-
-            mverr = 0;
-
-            //actualizare coordonate
-            for (i = snake.dim; i > 0; i--){
-                snake.p[i].x = snake.p[i - 1].x;
-                snake.p[i].y = snake.p[i - 1].y;
+            if(food.x <= 2){
+                food.x = 3;
+            }
+            if (food.y <= 2){
+                food.y = 3;
             }
 
-            snake.last_x = snake.p[snake.dim].x;
-            snake.last_y = snake.p[snake.dim].y;
+            do{
 
-            timeout(UNITY - snake.speed);
+                fooderr = 0;
+                for (i = 0; i < snake.dim; i++){
+                    if (food.x == snake.p[i].x
+                        && food.y == snake.p[i].y){
 
+                            food.x++;
 
-            //adaugare mancare
-            if(hungry){
-
-                hungry = 0;
-
-                do{
-
-                    fooderr = 0;
-
-                    srand(time(NULL));
-
-                    food.x = rand() % gboard.nr_x;
-                    food.y = rand() % gboard.nr_y;
-
-                    if(food.x <= 2 || food.y <= 2){
-                        fooderr = 1;
-                        continue;
-                    }
-
-                    for (i = 0; i < snake.dim; i++){
-
-                        if (food.x == snake.p[i].x
-                            && food.y == snake.p[i].y){
-
-                                fooderr = 1;
-                                break;
-
+                            if (food.x >= gboard.nr_x - 1){
+                                food.x = food.y;
                             }
-                        }
+                            fooderr = 1;
+                            break;
 
-                    }while (fooderr == 1);
+                    }
+                }
+            }while(fooderr);
 
-                    mvaddch(food.y, food.x, '*');
+            mvaddch(food.y, food.x, '*');
+        }
+            snake.input = getch();
+            snake.input = tolower (snake.input);
 
+            /*
+            Prindere eroare:
+            daca sarpele merge spre stanga si utilizatorul
+            apasa dreapta
+            */
+        {
+
+            if (snake.input == 'w'
+                && snake.lastinput == 's'){
+                    mverr = 1;
                 }
 
+            if (snake.input == 'a'
+                && snake.lastinput == 'd'){
+                    mverr = 1;
+                }
 
-                snake.input = getch();
-                snake.input = tolower (snake.input);
+            if (snake.input == 's'
+                && snake.lastinput == 'w'){
+                    mverr = 1;
+                }
 
-                /*
-                Prindere eroare:
-                daca sarpele merge spre stanga si utilizatorul
-                apasa dreapta
-                */
-            {
+            if (snake.input == 'd'
+                && snake.lastinput =='a'){
+                    mverr = 1;
+                }
+        }
 
-                if (snake.input == 'w'
-                    && snake.lastinput == 's'){
-                        mverr = 1;
-                    }
-
-                if (snake.input == 'a'
-                    && snake.lastinput == 'd'){
-                        mverr = 1;
-                    }
-
-                if (snake.input == 's'
-                    && snake.lastinput == 'w'){
-                        mverr = 1;
-                    }
-
-                if (snake.input == 'd'
-                    && snake.lastinput =='a'){
-                        mverr = 1;
-                    }
-            }
-
-            //daca s-a introdus ceva corect
-            if (snake.input != ERR && !mverr){
-                snake.lastinput = snake.input;
-            }
+        //daca s-a introdus ceva corect
+        if (snake.input != ERR && !mverr){
+            snake.lastinput = snake.input;
+        }
 
 
 
-            //daca apar erori de miscare
-            if (mverr){
+        //daca apar erori de miscare
+        if (mverr){
 
-                //sa se miste in aceeasi directie
-                snake.input = ERR;
-            }
+            //sa se miste in aceeasi directie
+            snake.input = ERR;
+        }
 
 
-            if (snake.input == 'q'){
-                quit = 1;
-            }
+        if (snake.input == 'q'){
+            quit = 1;
+        }
 
-            if (quit){
+        if (quit){
+            break;
+        }
+
+
+
+
+        //input
+        switch (snake.input){
+            case 'a':
+                if(snake.p[HEAD].x > 2){
+                    snake.p[HEAD].x --;
+                }else{
+                    over = 1;
+
+                }
+            break;
+
+            case 's':
+                if (snake.p[HEAD].y < gboard.nr_y + 1){
+                    snake.p[HEAD].y++;
+                }else{
+                    over = 1;
+                }
+            break;
+
+            case 'd':
+                if (snake.p[0].x < gboard.nr_x){
+                    snake.p[0].x++;
+                }else{
+                    over = 1;
+                }
                 break;
-            }
+            case 'w':
+                if (snake.p[0].y > 1){
+                    snake.p[0].y --;
+                }else{
+                    over = 1;
 
+                }
+            break;
 
-
-
-            //input
-            switch (snake.input){
-                case 'a':
+            default:
+                switch (snake.lastinput){
+                    case 'a':
                     if(snake.p[HEAD].x > 2){
                         snake.p[HEAD].x --;
                     }else{
                         over = 1;
 
                     }
-                break;
+                    break;
 
-                case 's':
+                    case 's':
                     if (snake.p[HEAD].y < gboard.nr_y + 1){
                         snake.p[HEAD].y++;
                     }else{
                         over = 1;
                     }
-                break;
+                    break;
 
-                case 'd':
+                    case 'd':
                     if (snake.p[0].x < gboard.nr_x){
                         snake.p[0].x++;
                     }else{
                         over = 1;
                     }
                     break;
-                case 'w':
+                    case 'w':
                     if (snake.p[0].y > 1){
                         snake.p[0].y --;
                     }else{
                         over = 1;
 
                     }
-                break;
-
-                default:
-                    switch (snake.lastinput){
-                        case 'a':
-                        if(snake.p[HEAD].x > 2){
-                            snake.p[HEAD].x --;
-                        }else{
-                            over = 1;
-
-                        }
-                        break;
-
-                        case 's':
-                        if (snake.p[HEAD].y < gboard.nr_y + 1){
-                            snake.p[HEAD].y++;
-                        }else{
-                            over = 1;
-                        }
-                        break;
-
-                        case 'd':
-                        if (snake.p[0].x < gboard.nr_x){
-                            snake.p[0].x++;
-                        }else{
-                            over = 1;
-                        }
-                        break;
-                        case 'w':
-                        if (snake.p[0].y > 1){
-                            snake.p[0].y --;
-                        }else{
-                            over = 1;
-
-                        }
-                        break;
-
-                    }
                     break;
 
                 }
+                break;
 
-        //verificare daca s-a lovit de el
-        for (i = 1; i < snake.dim; i ++){
-            if(snake.p[HEAD].x == snake.p[i].x &&
-                snake.p[HEAD].y == snake.p[i].y){
-                        over = 1;
-            }
-        }
-
-
-        if (over){
-            mvaddstr(10, gboard.nr_x / 3, "GAME OVER");
-            getch();
-            break;
-        }
-
-        //a ajuns sa manance
-        if (snake.p[HEAD].x == food.x &&
-            snake.p[HEAD].y == food.y){
-
-                hungry = 1;
-
-                snake.dim++;
-
-                if (!snake.dim % 2){
-                    if (snake.dim < 5){
-                        speed_augment += 5;
-                    }
-                    if (snake.dim >= 5 && snake.dim < 20){
-                        speed_augment += 4;
-                    }
-                    if (snake.dim >= 20){
-                        speed_augment += 3;
-                    }
-                    if (snake.speed + speed_augment >= 1000){
-                        snake.speed++;
-                    }else{
-                        snake.speed += speed_augment;
-                    }
-                }
-                snake.p[snake.dim].x = snake.last_x;
-                snake.p[snake.dim].y = snake.last_y;
-
-
-        }
-
-
-        //sterge coada + inaintare cap
-            if(! hungry){
-                mvaddch(snake.last_y, snake.last_x, ' ');
-            }else{
-                mvaddch(snake.last_y, snake.last_x, 'O');
             }
 
-
-        mvaddch(snake.p[0].y, snake.p[0].x, 'O');
-
-
-        refresh();
-
+    //verificare daca s-a lovit de el
+    for (i = 1; i < snake.dim; i ++){
+        if(snake.p[HEAD].x == snake.p[i].x &&
+            snake.p[HEAD].y == snake.p[i].y){
+                    over = 1;
         }
-        break;
-}
-
-{
-    case 0:
-        endwin();
-        exit(1);
-}
     }
 
-}
+
+    if (over){
+        mvaddstr(10, gboard.nr_x / 3, "GAME OVER");
+        timeout(-1);
+        snake.speed = 0;
+        getch();
+        break;
+    }
+
+    //a ajuns sa manance
+    if (snake.p[HEAD].x == food.x &&
+        snake.p[HEAD].y == food.y){
+
+            hungry = 1;
+
+            snake.dim++;
+
+            //daca mananca
+            //creste viteza
+            if (!snake.dim % 2){
+                if (snake.dim < 5){
+                    speed_augment += 10;
+                }
+                if (snake.dim >= 5 && snake.dim < 20){
+                    speed_augment += 8;
+                }
+                if (snake.dim >= 20){
+                    speed_augment += 4;
+                }
+                if (snake.speed + speed_augment >= 1000){
+                    snake.speed += 2;
+                }else{
+                    snake.speed += speed_augment;
+                }
+            }
+
+            if(snake.speed >= 1000){
+                snake.speed = 800;
+            }
+
+            snake.p[snake.dim].x = snake.last_x;
+            snake.p[snake.dim].y = snake.last_y;
+
+
+    }
+
+
+    //sterge coada + inaintare cap
+        if(! hungry){
+            mvaddch(snake.last_y, snake.last_x, ' ');
+        }else{
+            mvaddch(snake.last_y, snake.last_x, 'O');
+        }
+
+
+    mvaddch(snake.p[0].y, snake.p[0].x, 'O');
+
+
+    refresh();
+
+    }
+
+
+
+
     //inchidere
     endwin();
 
