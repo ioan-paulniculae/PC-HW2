9ba02046eda8e9d5a635ddabb211596c17eb3182
Niculae Ioan-Paul <niculae.paul@gmail.com>
2014-12-22 16:11:22 +0200

    Randomized food

--- tema2.c
+++ tema2.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <ctype.h>
 #include <ncurses.h>
 
 #define SPATIERE 0
 #define NO_STOP 1
 #define HEAD 0
+#define UNITY 1000
 
 typedef struct {
     unsigned x;
@@ -20,6 +21,8 @@ typedef struct {
     unsigned last_y;
 
     unsigned dim;
+    unsigned speed;
+
     char input;
     char lastinput;
 
@@ -104,10 +107,17 @@ int main(){
     WINDOW *wnd = initscr();
     BOARD gboard;
     SNAKE snake;
+    PUNCT food;
 
-    unsigned over = 0;
-    unsigned quit = 0;
+    unsigned over = 0;      //daca ajunge 1, game over
+    unsigned quit = 0;      //daca ajunge 1, trebuie iesit
+    unsigned hungry = 1;    //1= daca nu e mancare pe tabla
 
+    //erors
+    unsigned mverr = 0;     //daca ajunge 1, eroare de miscare
+    unsigned fooderr = 0;   //1 - daca eroare la generare mancare
+
+    unsigned i;
 
 
     init_window();
@@ -130,29 +140,103 @@ int main(){
     snake.p = (PUNCT *) malloc (gboard.nr_x * gboard.nr_y * sizeof (PUNCT));
 
     //pozitionare initiala
-    snake.p[0].x = gboard.nr_x  / 3;
-    snake.p[0].y = gboard.nr_y / 3 ;
+    snake.p[HEAD].x = gboard.nr_x  / 3;
+    snake.p[HEAD].y = gboard.nr_y / 3 ;
+    mvaddch(snake.p[HEAD].y, snake.p[HEAD].x, 'o');
 
+    //dimensiune initiala
     snake.dim = 1;
 
-    mvaddch(snake.p[0].y, snake.p[0].x, '0');
+    //viteza initiala
+    snake.speed = 350;
 
 
     while (NO_STOP){
 
+        mverr = 0;
         snake.last_x = snake.p[snake.dim - 1].x;
         snake.last_y = snake.p[snake.dim - 1].y;
 
-        timeout(500);
+        timeout(UNITY - snake.speed);
+
+
+        //adaugare mancare
+        if(hungry){
+
+            hungry = 0;
+
+            do{
+
+                fooderr = 0;
+
+                srand(time(NULL));
+
+                food.x = rand() % gboard.nr_x;
+                food.y = rand() % gboard.nr_y;
+
+                for (i = 0; i < snake.dim; i++){
+
+                    if (food.x == snake.p[i].x
+                        && food.y == snake.p[i].y){
+
+                            fooderr = 1;
+                            break;
+
+                    }
+                }
+
+            }while (fooderr == 1);
+
+            mvaddch(food.y, food.x, '*');
+
+        }
+
 
         snake.input = getch();
 
-        if (snake.input != ERR){
+        /*
+        Prindere eroare:
+        daca sarpele merge spre stanga si utilizatorul
+        apasa dreapta
+        */
+    {
+
+        if (snake.input == 'w'
+            && snake.lastinput == 's'){
+                mverr = 1;
+            }
+
+        if (snake.input == 'a'
+            && snake.lastinput == 'd'){
+                mverr = 1;
+            }
+
+        if (snake.input == 's'
+            && snake.lastinput == 'w'){
+                mverr = 1;
+            }
+
+        if (snake.input == 'd'
+            && snake.lastinput =='a'){
+                mverr = 1;
+            }
+    }
+
+        //daca s-a introdus ceva corect
+        if (snake.input != ERR && !mverr){
             snake.lastinput = snake.input;
         }
 
         snake.input = tolower (snake.input);
 
+
+
+        //daca apar erori de miscare
+        if (mverr){
+
+            //sa se miste in aceeasi directie
+            snake.input = ERR;
+        }
 
 
         if (snake.input == 'q'){
@@ -164,8 +248,7 @@ int main(){
         }
 
         if (over){
-            clear();
-            mvaddstr(30, 30, "GAME OVER");
+            mvaddstr(10, 30, "GAME OVER");
             getch();
             break;
         }
@@ -205,7 +288,7 @@ int main(){
                 }
                 break;
 
-            case ERR:
+            default:
                 switch (snake.lastinput){
                 case 'a':
                 if(snake.p[HEAD].x > 2){
@@ -246,9 +329,20 @@ int main(){
         }
 
 
+        if (snake.p[HEAD].x == food.x &&
+            snake.p[HEAD].y == food.y){
+                hungry = 1;
+            
+            }
+
+
         //sterge coada + inaintare cap
+
         mvaddch(snake.last_y, snake.last_x, ' ');
-        mvaddch(snake.p[0].y, snake.p[0].x, '0');
+        mvaddch(snake.p[0].y, snake.p[0].x, 'o');
+
+
+
 
         refresh();
 
